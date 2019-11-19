8514d879464407f288ae14f05399e39eeb59512c
Guillaume Grimault <ggrimault@iutbayonne.univ-pau.fr>
2019-11-19 14:46:27 +0100

    TP_2 Mise en place des Vues

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -10,36 +10,56 @@ class ProStageController extends AbstractController
 {
     public function accueil()
     {
-
-        return new Response("<html><body><h1>Bienvenue sur la page d'accueil de Prostages</h1></body></html>");
-        
+    //méthode Response
         /*
+        return new Response("<html><body><h1>Bienvenue sur la page d'accueil de Prostages</h1></body></html>");
+        */
+
+    //méthode Templates
         return $this->render('pro_stage/accueil.html.twig', [
             'controller_name' => 'ProStageController_accueil',
         ]);
-        */
+        
     }
 
     public function entreprises()
     {
+    //méthode Response
+        /*
         return new Response("<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>");
+        */
+
+    //méthode Templates
+        return $this->render('pro_stage/entreprises.html.twig', [
+            'controller_name' => 'ProStageController_entreprises',
+        ]);
     }
 
     public function formations()
     {
+    //méthode Response
+        /*
         return new Response("<html><body><h1>Cette page affichera la liste des formations de l'IUT</h1></body></html>");
+        */
+
+    //méthode Templates
+        return $this->render('pro_stage/formations.html.twig', [
+            'controller_name' => 'ProStageController_formations',
+        ]);
+    
     }
 
     public function stages($id)
     {
+    //méthode Response
+        //return new Response("<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant". $id."</h1></body></html>");
 
-        return new Response("<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant". $id."</h1></body></html>");
-
-        /*
+    //méthode Templates
         return $this->render('pro_stage/stages.html.twig', [
-            'controller_name' => 'ProStageController_stages', 'id' => $id,
+            'controller_name' => 'ProStageController_stages',
+            'identifiant' => $id,
         ]);
-        */
+        
         
     }
     
