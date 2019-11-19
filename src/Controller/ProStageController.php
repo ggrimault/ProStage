cbaedadedc1bdda627b3c38fe9237ec186956918
Guillaume Grimault <ggrimault@iutbayonne.univ-pau.fr>
2019-11-19 13:19:02 +0100

    Mise en place controlleur et templates

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -4,16 +4,57 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProStageController extends AbstractController
 {
-    /**
-     * @Route("/pro/stage", name="pro_stage")
-     */
-    public function index()
+    public function accueil()
     {
-        return $this->render('pro_stage/index.html.twig', [
+
+        return new Response("<html><body><h1>Bienvenue sur la page d'accueil de Prostages</h1></body></html>");
+        
+        /*
+        return $this->render('pro_stage/accueil.html.twig', [
             'controller_name' => 'ProStageController_accueil',
         ]);
+        */
     }
+
+    public function entreprises()
+    {
+
+        return new Response("<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>");
+
+        /*
+        return $this->render('pro_stage/entreprises.html.twig', [
+            'controller_name' => 'ProStageController_entreprises',
+        ]);
+        */
+    }
+
+    public function formations()
+    {
+
+        return new Response("<html><body><h1>Cette page affichera la liste des formations de l'IUT</h1></body></html>");
+
+        /*
+        return $this->render('pro_stage/formations.html.twig', [
+            'controller_name' => 'ProStageController_formations',
+        ]);
+        */
+    }
+
+    public function stages($id)
+    {
+
+        return new Response("<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant ". $id ." </h1></body></html>");
+
+        /*
+        return $this->render('pro_stage/stages.html.twig', [
+            'controller_name' => 'ProStageController_stages', 'id' => $id,
+        ]);
+        */
+    }
+
+
 }
