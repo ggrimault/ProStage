b688704fc8ff6f4177597458dfb03c183543714f
Guillaume Grimault <ggrimault@iutbayonne.univ-pau.fr>
2019-11-19 13:28:55 +0100

    Mise en place des Responses

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
 
 class ProStageController extends AbstractController
 {
@@ -22,39 +21,27 @@ class ProStageController extends AbstractController
 
     public function entreprises()
     {
-
         return new Response("<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>");
-
-        /*
-        return $this->render('pro_stage/entreprises.html.twig', [
-            'controller_name' => 'ProStageController_entreprises',
-        ]);
-        */
     }
 
     public function formations()
     {
-
         return new Response("<html><body><h1>Cette page affichera la liste des formations de l'IUT</h1></body></html>");
-
-        /*
-        return $this->render('pro_stage/formations.html.twig', [
-            'controller_name' => 'ProStageController_formations',
-        ]);
-        */
     }
 
     public function stages($id)
     {
 
-        return new Response("<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant ". $id ." </h1></body></html>");
+        return new Response("<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant". $id."</h1></body></html>")
 
         /*
         return $this->render('pro_stage/stages.html.twig', [
             'controller_name' => 'ProStageController_stages', 'id' => $id,
         ]);
         */
+        
     }
+    
 
 
 }
