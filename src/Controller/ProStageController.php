31c28594660657d0be0a0eb823f01f34a699e899
Guillaume Grimault <ggrimault@iutbayonne.univ-pau.fr>
2019-12-20 16:02:57 +0100

    Injection

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -8,26 +8,17 @@ use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Entreprise;
 use App\Entity\Formation;
 use App\Entity\Stage;
+use App\Repository\EntrepriseRepository;
+use App\Repository\FormationRepository;
+use App\Repository\StageRepository;
 
 class ProStageController extends AbstractController
 {
-    public function accueil()
+    public function accueil(StageRepository $repositoryStage)
     {
     //Traitement métier/controlleur
-        $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
-        $listeFormations = $repositoryFormation->findAll();
-
-        $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
         $listeStages = $repositoryStage->findAll();
-
-        $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
-        $listeFormations = $repositoryFormation->findAll();
-
-    //méthode Response
-        /*
-        return new Response("<html><body><h1>Bienvenue sur la page d'accueil de Prostages</h1></body></html>");
-        */
-
+   
     //méthode Templates
         return $this->render('pro_stage/accueil.html.twig', [
             'controller_name' => 'ProStageController_accueil',
@@ -36,17 +27,10 @@ class ProStageController extends AbstractController
         ]);
     }
 
-    public function entreprises()
+    public function entreprises(EntrepriseRepository $repositoryEntreprise)
     {
     //Traitement métier/controlleur
-        $repositoryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
         $listeEntreprises = $repositoryEntreprise->findAll();
-
-
-    //méthode Response
-        /*
-        return new Response("<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>");
-        */
 
     //méthode Templates
         return $this->render('pro_stage/entreprises.html.twig', [
@@ -56,81 +40,42 @@ class ProStageController extends AbstractController
         ]);
     }
 
-    public function formations()
+    public function formations(FormationRepository $repositoryFormation)
     {
     //Traitement métier/controlleur
-
-        $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
         $listeFormations = $repositoryFormation->findAll();
 
 
     //Envoie à la Vue
-
-        //méthode Response
-            /*
-            return new Response("<html><body><h1>Cette page affichera la liste des formations de l'IUT</h1></body></html>");
-            */
-
-        //méthode Templates
-            return $this->render('pro_stage/formations.html.twig', [
-                'controller_name' => 'ProStageController_formations',
-                'name' => 'formations',
-                'listeFormations' => $listeFormations,
-            ]);
+        return $this->render('pro_stage/formations.html.twig', [
+            'controller_name' => 'ProStageController_formations',
+            'name' => 'formations',
+            'listeFormations' => $listeFormations,
+        ]);
     }
 
-    public function stages($id)
+    public function stages(Stage $stage)
     {
-    //Traitement métier/controlleur
-
-        $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
-        $stage = $repositoryStage->find($id);
-
-    //Envoie à la Vue
-
-        //méthode Response
-            /*
-            return new Response("<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant". $id."</h1></body></html>");
-            */
-
         //méthode Templates
             return $this->render('pro_stage/stages.html.twig', [
                 'controller_name' => 'ProStageController_stages',
-                'identifiant' => $id,
                 'name' => 'stage',
                 'stage' => $stage,
             ]);
     }
     
-    public function parEntreprise($id)
+    public function parEntreprise(Entreprise $entreprise)
     {
-    //Traitement métier/controlleur
-
-        $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
-        $repositoryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
-
-        $entreprise = $repositoryEntreprise->find($id);
-        $stages = $repositoryStage->findBy(["entreprise"=>$id]);
-
-
-    
     //Envoie à la vue
         return $this->render('pro_stage/parEntreprise.html.twig', [
             'controller_name' => 'ProStageController_parEntreprise',
             'name' => 'parEntreprise',
             'infoEntreprise' => $entreprise,
-            'listeStages' => $stages,
         ]);
     }
 
-    public function parFormation($id)
+    public function parFormation(Formation $formation)
     {
-    //Traitement métier/controlleur
-
-        $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
-        $formation = $repositoryFormation->find($id);
-
-
     //Envoie à la vue
         return $this->render('pro_stage/parFormation.html.twig', [
             'controller_name' => 'ProStageController_parFormation',
